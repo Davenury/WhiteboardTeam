36e01f0d03f48443f6048e5564fe885b851dc346
Dawid Dębowski <dawid199960@gmail.com>
2021-02-28 12:26:53 +0100

    Whiteboard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  let wt = null;
+
+  useEffect(() => {
+    wt = new window.api.WhiteboardTeam('#wt-container', {
+      clientId: process.env.REACT_APP_CLIENT_ID,
+      boardCode: process.env.REACT_APP_WHITEBOARD_CODE
+    });
+  }, [])
+
+  const getUser = () => {
+    if(wt != null){
+      wt.getCurrentUser()
+        .then(user => console.log(user));
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{width: "100%", height: "100%"}}>
+      <div style={{width: "100%", height: "600px"}} id="wt-container"></div>
+      <button onClick={getUser}>Click!</button>
     </div>
   );
 }
